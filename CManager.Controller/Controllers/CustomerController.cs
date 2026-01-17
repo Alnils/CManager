485a377d80b5a4044e5407efe37d105401b18d62
alnils <albin.nilsson.se@gmail.com>
2026-01-17 17:55:44 +0100

    created controller logic for create customer

---
+++ CManager.Controller/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using CManager.Controller.Interfaces;
+using CManager.Service.Interfaces;
+using CManager.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Controller.Controllers
+{
+    internal class CustomerController : ICustomerController
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public void CreateCustomer( 
+            string firstName, 
+            string lastName, 
+            string email, 
+            string phone, 
+            string street, 
+            string zipcode, 
+            string city
+        )
+        {
+            var customer = new Customer 
+            { 
+                FirstName = firstName, 
+                LastName = lastName, 
+                Email = email, 
+                Phone = phone, 
+                Adress = new Adress 
+                { 
+                    Street = street, 
+                    ZipCode = zipcode, 
+                    City = city 
+                } 
+            };
+            _customerService.Create(customer);
+        }
+
+    
+    }
+}
---
+++ CManager.Controller/Interfaces/ICustomerController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Controller.Interfaces
+{
+    public interface ICustomerController
+    {
+        public void CreateCustomer(string firstName, string lastName, string email, string phone, string street, string zipcode, string city);
+    }
+}
