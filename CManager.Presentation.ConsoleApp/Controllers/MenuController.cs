6b6e818dece611a139f8322c291a879cebba4e2c
alnils <albin.nilsson.se@gmail.com>
2026-01-19 21:12:00 +0100

    added hosting and controller

---
+++ CManager.Presentation.ConsoleApp/Controllers/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Presentation.ConsoleApp.Controllers
+{
+    internal class MenuController
+    {
+        public MenuController() {}
+
+        public void Run() 
+        {
+            Console.WriteLine("hej from controller");
+        }
+    }
+}
--- CManager.Presentation.ConsoleApp/Program.cs
+++ CManager.Presentation.ConsoleApp/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("hej");
+﻿using CManager.Presentation.ConsoleApp.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<MenuController>();
+
+var app = builder.Build();
+
+app.Services.GetRequiredService<MenuController>().Run();
